261c9b601cde79ba0683b7cd273d86e243e6b3de
Josh Gillies <github@josh.gl>
2017-04-03 05:27:05 +1000

    hacking around

--- factory.js
+++ factory.js
@@ -1,7 +1,10 @@
 var microcomponent = require('./')
+var slice = Array.prototype.slice
 
 function mcfactory (opts) {
   return function wrapComponent () {
+    var args = slice.call(arguments)
+    if (args.length) return microcomponent(opts).apply(null, args)
     return microcomponent(opts)
   }
 }
